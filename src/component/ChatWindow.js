35b5f1b2a6c6acbc32c0783812275436d6e0ce93
Sachin adk <168266874+sachinadk2011@users.noreply.github.com>
2025-12-05 21:51:41 +0545

    feat: enhance message sending logic to prevent empty messages and improve paste handling

--- src/component/ChatWindow.js
+++ src/component/ChatWindow.js
@@ -95,16 +95,25 @@ const ChatWindow = () => {
     }
   };
 
+  
   // Listen globally for paste
   window.addEventListener("paste", handlePaste);
+  
+        
 
-  return () => window.removeEventListener("paste", handlePaste);
+  return () => {
+    window.removeEventListener("paste", handlePaste);
+   
+  };
 }, []);
 
 console.log("files to be sent:", images, images);
    const SendMessage =async(e)=>{
       e.preventDefault();
       const formData = new FormData();
+      if (images.length === 0 && e.target.elements.message.value.trim() === "") {
+        return; // Don't send empty messages
+      }
       
       const message = e.target.elements.message.value;
       if (message) formData.append('message', message);
@@ -131,6 +140,7 @@ console.log("files to be sent:", images, images);
   
 }
     
+
     return(
         <>
        
@@ -243,7 +253,9 @@ console.log("files to be sent:", images, images);
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                </svg>
             </button>
-            <button type="submit" className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none" >
+            <button type="submit" 
+            
+            className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none" >
                <span className="font-bold">Send</span>
                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90">
                   <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
