d4f5eaa80193694f7c4857a2053fa9ea42f7b007
Sachin adk <168266874+sachinadk2011@users.noreply.github.com>
2025-12-07 13:21:39 +0545

    feat: update socket connection to include polling transport for improved real-time communication

--- src/server/socket.js
+++ src/server/socket.js
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 // connect to your backend
 const socket = io(process.env.REACT_APP_URL, {
   withCredentials: true,   // send cookies if needed
-  transports: ["websocket"], // ensures real-time connection
+  transports: ["websocket", "polling"], // ensures real-time connection
 });
 
 export default socket;
